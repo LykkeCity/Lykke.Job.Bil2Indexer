ccc87c570310e3b8f0a6327bcad47c4ce7fc5ad1
Aleksandr Belousov <alexandr.belousov@lykke.com>
2019-04-12 10:39:04 +0400

    Added template for verifying tool

---
+++ tools/Lykke.Job.Bil2Indexer.VerifyingTool/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using Lykke.Job.Bil2Indexer.Domain.Repositories;
+
+namespace Lykke.Job.Bil2Indexer.VerifyingTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger fromBlock = BigInteger.Parse(args[0]);
+            BigInteger toBlock = BigInteger.Parse(args[1]);
+            string blockchainType = args[2];
+
+            IBalanceActionsRepository balanceActionsRepository;
+            IBlockHeadersRepository blockHeadersRepository;
+            ICoinsRepository coinsRepository;
+            ICrawlersRepository crawlersRepository;
+            ITransactionsRepository transactionsRepository;
+            IChainHeadsRepository chainHeadsRepository;
+        }
+    }
+}
