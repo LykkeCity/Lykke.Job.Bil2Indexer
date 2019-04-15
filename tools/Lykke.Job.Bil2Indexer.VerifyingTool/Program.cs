a1a02b1af39ca331c0248909833bac18867115d5
Aleksandr Belousov <alexandr.belousov@lykke.com>
2019-04-15 11:47:46 +0400

    Completed get block async method

--- tools/Lykke.Job.Bil2Indexer.VerifyingTool/Program.cs
+++ tools/Lykke.Job.Bil2Indexer.VerifyingTool/Program.cs
@@ -12,12 +12,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
+using Lykke.Bil2.SharedDomain;
+using QBitNinja.Client.Models;
 using BlockHeader = Lykke.Job.Bil2Indexer.Domain.BlockHeader;
 
 namespace Lykke.Job.Bil2Indexer.VerifyingTool
 {
     class Program
     {
+        private static int _limit = 100;
+
         static async Task Main(string[] args)
         {
             BigInteger fromBlock = BigInteger.Parse(args[0]);
@@ -66,13 +70,6 @@ namespace Lykke.Job.Bil2Indexer.VerifyingTool
 
                     #region Check Block Indexation 
 
-                    //var blockResult = block.Result;
-                    //var ledger = blockResult.Ledger.Parse();
-                    //var transactionsCount = ledger.Transactions.Length;
-                    //var ledgerHash = blockResult.LedgerHash;
-                    //var ledgerIndex = blockResult.LedgerIndex;
-                    //var ledgerParentHash = ledger.ParentHash;
-
                     AssertEqual(indexedBlock.BlockchainType, realBlock.BlockchainType);
                     AssertEqual(indexedBlock.Id, realBlock.Id);
                     AssertEqual(indexedBlock.IsExecuted, true);
@@ -98,6 +95,7 @@ namespace Lykke.Job.Bil2Indexer.VerifyingTool
                                 transactionsRepository.GetTransferCoinsTransactionsOfBlockAsync(
                                     blockchainType,
                                     indexedBlock.Id,
+                                    _limit,
                                     continuation);
 
                             continuation = paginationResponse.Continuation;
@@ -116,68 +114,113 @@ namespace Lykke.Job.Bil2Indexer.VerifyingTool
                         var orderedTransactions = transfers.OrderBy(x => x.TransactionNumber).ToArray();
                         var (realCoinTransfers, realFailedEvents) =
                             await adapter.GetTransactionsForBlockAsync(currentBlockNumber);
+                        var orderedRealCoinTransfers = realCoinTransfers.OrderBy(x => x.TransactionNumber).ToArray();
+                        var orderedRealFailedEvents = realFailedEvents.OrderBy(x => x.TransactionNumber).ToArray();
 
-                        for (int j = 0; j < orderedTransactions.Length; j++)
+                        AssertEqual(orderedTransactions.Length, realCoinTransfers.Count());
+                        AssertEqual(failedTransfers.Count, realFailedEvents.Count());
+
+
+                        for (int j = 0; j < orderedRealCoinTransfers.Length; j++)
                         {
-                            //For Coin Transfers
-                            //orderedTransactions[i].BlockId;
-                            //orderedTransactions[i].Fees;
-                            //orderedTransactions[i].IsIrreversible;
-                            //orderedTransactions[i].SpentCoins;
-                            //orderedTransactions[i].ReceivedCoins;
+                            var orderedTransactionIndexed = orderedTransactions[j];
+                            var orderedTransactionReal = orderedRealCoinTransfers[j];
 
-                            //AssertEqual((uint) orderedTransactions[j].TransactionNumber, transactions[j].Sequence);
-                            //AssertEqual(orderedTransactions[j].TransactionId, transactions[j].TransactionType);
-                            //AssertEqual(orderedTransactions[j].TransactionId, transactions[j].);
+                            AssertEqual(orderedTransactionIndexed.TransactionNumber, orderedTransactionReal.TransactionNumber);
+                            AssertEqual(orderedTransactionIndexed.TransactionId, orderedTransactionReal.TransactionId);
+                            AssertEqual(orderedTransactionIndexed.BlockId, orderedTransactionReal.BlockId);
+                            AssertEqual(orderedTransactionIndexed.IsIrreversible ?? false, orderedTransactionReal.IsIrreversible ?? false);
 
-                            //var transfer = orderedTransactions[i]
-                            //    .BalanceChanges
-                            //    .FirstOrDefault(x => x.Address == transactions[j].Account &&
-                            //                         x.Value.transactions[j].Amount.);
-                            //AssertEqual(orderedTransactions[j].Fees, transactions[j].Fee);
+                            var spentCoinsReal = orderedTransactionReal.SpentCoins.ToArray();
+                            var spentCoinsIndexed = orderedTransactionIndexed.SpentCoins.ToArray();
+
+                            for (int spentCoinIndex = 0; spentCoinIndex < spentCoinsReal.Length; spentCoinIndex++)
+                            {
+                                var spentCoinIndexed = spentCoinsIndexed[spentCoinIndex];
+                                var spentCoinReal = spentCoinsReal[spentCoinIndex];
+
+                                AssertEqual(spentCoinIndexed.CoinNumber, spentCoinReal.CoinNumber);
+                                AssertEqual(spentCoinIndexed.TransactionId, spentCoinReal.TransactionId);
+                            }
+
+                            var receivedCoinsReal = orderedTransactionReal.ReceivedCoins.ToArray();
+                            var receivedCoinsIndexed = orderedTransactionIndexed.ReceivedCoins.ToArray();
+
+                            for (int receivedCoinIndex = 0; receivedCoinIndex < receivedCoinsReal.Length; receivedCoinIndex++)
+                            {
+                                var receivedCoinIndexed = receivedCoinsIndexed[receivedCoinIndex];
+                                var receivedCoinReal = receivedCoinsReal[receivedCoinIndex];
+
+                                AssertEqual(receivedCoinIndexed.CoinNumber, receivedCoinReal.CoinNumber);
+                                AssertEqual(receivedCoinIndexed.Address, receivedCoinReal.Address);
+                                AssertEqual(receivedCoinIndexed.AddressNonce ?? 0, receivedCoinReal.AddressNonce ?? 0);
+                                AssertEqual(receivedCoinIndexed.Address, receivedCoinReal.Address);
+                                AssertEqual((int?)receivedCoinIndexed.AddressTagType ?? 0, 
+                                    (int?)receivedCoinReal.AddressTagType ?? 0);
+                                AssertEqual(receivedCoinIndexed.Value, receivedCoinReal.Value);
+                                AssertEqual(receivedCoinIndexed.Asset, receivedCoinReal.Asset);
+                            }
+
+                            var feesReal = orderedTransactionReal.Fees.ToArray();
+                            var feesIndexed = orderedTransactionIndexed.Fees.ToArray();
+
+                            for (int feesIndex = 0; feesIndex < feesReal.Length; feesIndex++)
+                            {
+                                var feeIndexed = feesIndexed[feesIndex];
+                                var feeReal = feesReal[feesIndex];
+
+                                AssertEqual(feeIndexed.Amount, feeReal.Amount);
+                                AssertEqual(feeIndexed.Asset, feeReal.Asset);
+                            }
                         }
                     }
+                    else if (blockchainIntegrationSettings.Capabilities.TransferModel ==BlockchainTransferModel.Amount)
+                    {
+                        //for (int j = 0; j < orderedTransactions.Length; j++)
+                        //{
+                        //    //For Coin Transfers
+                        //    //orderedTransactions[i].TransactionNumber;
+                        //    //orderedTransactions[i].BlockId;
+                        //    //orderedTransactions[i].Fees;
+                        //    //orderedTransactions[i].IsIrreversible;
+                        //    //orderedTransactions[i].SpentCoins;
+                        //    //orderedTransactions[i].TransactionId;
+                        //    //orderedTransactions[i].ReceivedCoins;
 
-                    //for (int j = 0; j < orderedTransactions.Length; j++)
-                    //{
-                    //    //For Coin Transfers
-                    //    //orderedTransactions[i].TransactionNumber;
-                    //    //orderedTransactions[i].BlockId;
-                    //    //orderedTransactions[i].Fees;
-                    //    //orderedTransactions[i].IsIrreversible;
-                    //    //orderedTransactions[i].SpentCoins;
-                    //    //orderedTransactions[i].TransactionId;
-                    //    //orderedTransactions[i].ReceivedCoins;
+                        //    //For Amount Transfers
+                        //    //orderedTransactions[i].TransactionNumber;
+                        //    //orderedTransactions[i].BlockId;
+                        //    //orderedTransactions[i].Fees;
+                        //    //orderedTransactions[i].IsIrreversible;
+                        //    //orderedTransactions[i].BalanceChanges;
+                        //    //orderedTransactions[i].TransactionId;
 
-                    //    //For Amount Transfers
-                    //    //orderedTransactions[i].TransactionNumber;
-                    //    //orderedTransactions[i].BlockId;
-                    //    //orderedTransactions[i].Fees;
-                    //    //orderedTransactions[i].IsIrreversible;
-                    //    //orderedTransactions[i].BalanceChanges;
-                    //    //orderedTransactions[i].TransactionId;
+                        //    // Ripple Transaction
+                        //    //transactions[i].Account;
+                        //    //transactions[i].Amount;
+                        //    //transactions[i].Destination;
+                        //    //transactions[i].DestinationTag;
+                        //    //transactions[i].Fee;
+                        //    //transactions[i].Flags;
+                        //    //transactions[i].LastLedgerSequence;
+                        //    //transactions[i].TransactionType;
+                        //    //transactions[i].Sequence;
 
-                    //    // Ripple Transaction
-                    //    //transactions[i].Account;
-                    //    //transactions[i].Amount;
-                    //    //transactions[i].Destination;
-                    //    //transactions[i].DestinationTag;
-                    //    //transactions[i].Fee;
-                    //    //transactions[i].Flags;
-                    //    //transactions[i].LastLedgerSequence;
-                    //    //transactions[i].TransactionType;
-                    //    //transactions[i].Sequence;
+                        //    //AssertEqual((uint) orderedTransactions[j].TransactionNumber, transactions[j].Sequence);
+                        //    //AssertEqual(orderedTransactions[j].TransactionId, transactions[j].TransactionType);
+                        //    ////AssertEqual(orderedTransactions[j].TransactionId, transactions[j].);
 
-                    //    //AssertEqual((uint) orderedTransactions[j].TransactionNumber, transactions[j].Sequence);
-                    //    //AssertEqual(orderedTransactions[j].TransactionId, transactions[j].TransactionType);
-                    //    ////AssertEqual(orderedTransactions[j].TransactionId, transactions[j].);
-
-                    //    //var transfer = orderedTransactions[i]
-                    //    //    .BalanceChanges
-                    //    //    .FirstOrDefault(x => x.Address == transactions[j].Account &&
-                    //    //                         x.Value.transactions[j].Amount.);
-                    //    //AssertEqual(orderedTransactions[j].Fees, transactions[j].Fee);
-                    //}
+                        //    //var transfer = orderedTransactions[i]
+                        //    //    .BalanceChanges
+                        //    //    .FirstOrDefault(x => x.Address == transactions[j].Account &&
+                        //    //                         x.Value.transactions[j].Amount.);
+                        //    //AssertEqual(orderedTransactions[j].Fees, transactions[j].Fee);
+                        //}
+                    }
+                    else
+                    {
+                        //Not Supported
+                    }
 
                     #endregion
                 }
@@ -304,6 +347,7 @@ namespace Lykke.Job.Bil2Indexer.VerifyingTool
                     transactionsRepository.GetFailedTransactionsOfBlockAsync(
                         blockchainType,
                         indexedBlock.Id,
+                        _limit,
                         continuation);
 
                 continuation = paginationResponse.Continuation;
@@ -368,19 +412,33 @@ namespace Lykke.Job.Bil2Indexer.VerifyingTool
             _ninjaClient = new QBitNinjaClient(ninjaUrl, network);
         }
 
-        public Task VerifyBlockAsync(BigInteger blockNumber)
+        public async Task<(IEnumerable<TransferCoinsTransactionExecutedEvent> coinTransfers, IEnumerable<TransactionFailedEvent> failedTransfers)> GetTransactionsForBlockAsync(BigInteger blockNumber)
         {
-            throw new NotImplementedException();
-        }
+            var listTransfered = new List<TransferCoinsTransactionExecutedEvent>();
 
-        public Task<(IEnumerable<TransferCoinsTransactionExecutedEvent> coinTransfers, IEnumerable<TransactionFailedEvent> failedTransfers)> GetTransactionsForBlockAsync(BigInteger blockNumber)
-        {
-            throw new NotImplementedException();
+
+            return (listTransfered, new TransactionFailedEvent[] {});
         }
 
         public async Task<BlockHeader> GetBlockAsync(BigInteger blockNumber)
         {
-            return null;
+            var block = await _ninjaClient.GetBlock(BlockFeature.Parse(blockNumber.ToString()), true, false);
+            var blockHash = block.Block.Header.GetHash().ToString();
+            var version = block.Block.Header.Version;
+            var size = 0;
+            var transactionCount = block.Block.Transactions.Count;
+            var previousBlockId = block.Block.Header.HashPrevBlock.ToString();
+
+            return new BlockHeader(blockHash,
+                version,
+                "Bitcoin",
+                (long)blockNumber,
+                block.Block.Header.BlockTime.DateTime,
+                size,
+                transactionCount,
+                previousBlockId,
+                BlockState.Executed
+                );
         }
     }
 }
