75554d51bbb67e867fdc9bc79f2aa0a5974f0372
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-04-16 23:48:54 +0300

    AssetInfo added

---
+++ src/Lykke.Job.Bil2Indexer.Domain/AssetInfo.cs
@@ -0,0 +1,18 @@
+﻿using Lykke.Bil2.SharedDomain;
+
+namespace Lykke.Job.Bil2Indexer.Domain
+{
+    public class AssetInfo
+    {
+        public string BlockchainType { get; }
+        public AssetId Id { get; }
+        public int Scale { get; }
+
+        public AssetInfo(string blockchainType, AssetId id, int scale)
+        {
+            BlockchainType = blockchainType;
+            Id = id;
+            Scale = scale;
+        }
+    }
+}
