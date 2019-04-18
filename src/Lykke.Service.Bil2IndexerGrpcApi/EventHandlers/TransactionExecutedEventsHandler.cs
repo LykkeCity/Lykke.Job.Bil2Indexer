57f1e3a5fe1a389a2d2750f1121ba28be14eb574
Sergey Polonsky <polonsky.sergey@hotmail.com>
2019-04-18 15:57:03 +0300

    Minimal grpc implementation

--- src/Lykke.Service.Bil2IndexerGrpcApi/EventHandlers/TransactionExecutedEventsHandler.cs
+++ src/Lykke.Service.Bil2IndexerGrpcApi/EventHandlers/TransactionExecutedEventsHandler.cs
@@ -4,15 +4,25 @@ using JetBrains.Annotations;
 using Lykke.Bil2.RabbitMq.Publication;
 using Lykke.Bil2.RabbitMq.Subscription;
 using Lykke.Job.Bil2Indexer.Contract.Events;
+using Lykke.Service.Bil2IndexerGrpcApi.Services;
 
 namespace Lykke.Service.Bil2IndexerGrpcApi.EventHandlers
 {
     [UsedImplicitly]
-    public class TransactionExecutedEventsHandler : IMessageHandler<TransactionExecutedEvent>
+    public class TransactionExecutedEventsHandler : IMessageHandler<Lykke.Job.Bil2Indexer.Contract.Events.TransactionExecutedEvent>
     {
-        public Task HandleAsync(TransactionExecutedEvent evt, MessageHeaders headers, IMessagePublisher _)
+        private readonly IndexerApiImpl _indexerApi;
+        public TransactionExecutedEventsHandler(IndexerApiImpl indexerApi)
+        {
+            _indexerApi = indexerApi;
+        }
+
+
+        public Task HandleAsync(Lykke.Job.Bil2Indexer.Contract.Events.TransactionExecutedEvent evt, MessageHeaders headers, IMessagePublisher _)
         {
             Console.WriteLine($"TransactionExecutedEvent :{Newtonsoft.Json.JsonConvert.SerializeObject(evt)}");
+
+            _indexerApi.Publish(evt);
 
             return Task.CompletedTask;
         }
---
+++ src/Lykke.Service.Bil2IndexerGrpcApi/Modules/GrpcModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Grpc.Core;
+using JetBrains.Annotations;
+using Lykke.Service.Bil2IndexerGrpcApi.Services;
+using Lykke.Service.Bil2IndexerGrpcApi.Settings;
+using Lykke.SettingsReader;
+
+namespace Lykke.Service.Bil2IndexerGrpcApi.Modules
+{
+    [UsedImplicitly]
+    public class GrpcModule : Module
+    {
+        private readonly AppSettings _settings;
+
+        public GrpcModule(IReloadingManager<AppSettings> settings)
+        {
+            _settings = settings.CurrentValue;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(ctx => new IndexerApiImpl())
+                .SingleInstance();
+
+            const int Port = 50051;
+            builder.Register(ctx =>
+                new Server
+                {
+                    Services = { IndexerApi.BindService(ctx.Resolve<IndexerApiImpl>()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                }
+            ).SingleInstance();
+        }
+    }
+}
