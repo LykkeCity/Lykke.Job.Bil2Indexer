a4c89a454adafd25906e4ec13a9e879af9e9b5dd
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-05-06 23:22:02 +0300

    logging improved

--- src/Lykke.Job.Bil2Indexer/Infrastructure/LogExtensions.cs
+++ src/Lykke.Job.Bil2Indexer/Infrastructure/LogExtensions.cs
@@ -8,7 +8,7 @@ namespace Lykke.Job.Bil2Indexer.Infrastructure
     {
         public static void LogLegacyMessage<TMessage>(this ILog log, TMessage message, MessageHeaders headers)
         {
-            log.Info("Message treated as legacy and ignored", new
+            log.Info(process: typeof(TMessage).Name, "Message treated as legacy and ignored", new
             {
                 Headers = headers, 
                 Message = message
