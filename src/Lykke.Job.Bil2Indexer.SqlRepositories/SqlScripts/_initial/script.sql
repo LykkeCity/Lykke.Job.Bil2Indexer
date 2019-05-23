b90382531b1804630357cb2e9c7484dc6186befe
some-dev <avolkov.dev@gmail.com>
2019-05-24 01:59:27 +0300

    fixed wrong index creation

--- src/Lykke.Job.Bil2Indexer.SqlRepositories/SqlScripts/_initial/script.sql
+++ src/Lykke.Job.Bil2Indexer.SqlRepositories/SqlScripts/_initial/script.sql
@@ -238,11 +238,11 @@ create table block_headers
 );
 
 create unique index block_headers_number_uindex
-    on block_headers (mined_at)   tablespace fast_space;
+    on block_headers (number)   tablespace fast_space;
 
     
 create index block_headers_mined_at_index
-    on block_headers (number)   tablespace fast_space;
+    on block_headers (mined_at)   tablespace fast_space;
 
 create table chain_heads
 (
