baacc4871e1fba04cdf653a6c76127d436a490a0
some-dev <avolkov.dev@gmail.com>
2019-05-30 20:56:39 +0300

    \u0000 jsonb sanitaze

--- src/Lykke.Job.Bil2Indexer.SqlRepositories/Repositories/Helpers/PgSanitazeHelper.cs
+++ src/Lykke.Job.Bil2Indexer.SqlRepositories/Repositories/Helpers/PgSanitazeHelper.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lykke.Job.Bil2Indexer.SqlRepositories.Repositories.Helpers
+﻿namespace Lykke.Job.Bil2Indexer.SqlRepositories.Repositories.Helpers
 {
     public static class PgSanitazeHelper
     {
         public static string SanitazeJson(this string source)
         {
             //https://stackoverflow.com/questions/31671634/handling-unicode-sequences-in-postgresql
-            return source?.Replace('\u0000'.ToString(), "");
+            return source?.Replace("\\u0000", "u0000");
         }
     }
 }
